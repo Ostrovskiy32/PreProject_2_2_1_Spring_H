599105c40945d9f45bb2d1e92c6048394be6c0d1
Ostrovskiy32 <134596413+Ostrovskiy32@users.noreply.github.com>
2023-08-23 23:43:18 +0300

    Update UserDao.java

--- src/main/java/hiber/dao/UserDao.java
+++ src/main/java/hiber/dao/UserDao.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface UserDao {
    void add(User user);
    List<User> listUsers();
-   User carUser(String model, int series);
+   User getByModelAndSeries(String model, int series);
 }
