aeb112b0fa8a8004e45411be8442ac68934db82d
Ostrovskiy32 <134596413+Ostrovskiy32@users.noreply.github.com>
2023-08-25 22:10:18 +0300

    Update Car.java

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -65,4 +65,16 @@ public class Car {
                 ", user=" + user +
                 '}';
     }
+        @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return series == car.series && Objects.equals(id, car.id) && Objects.equals(model, car.model) && Objects.equals(user, car.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, series, user);
+    }
 }
